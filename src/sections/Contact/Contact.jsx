be76f14098d633b0a7be09abb172197c43904f0b
Aaron Amoso <amosoaaron@gmail.com>
2024-11-24 01:40:32 -0700

    contact centering issue. centered in localhost but not when deployed

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -32,30 +32,32 @@ function Contact() {
   };
 
   return (
-    <section id="contact" className={styles.container}>
-      <h1 className="sectionTitle">Contact</h1>
-      <form action="" method="post" onSubmit={onSubmit}>
-        <div className="formGroup">
-          <label htmlFor="name" hidden>Name</label>
-          <input type="text" name="name" id="name" placeholder="Name" required />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="email" hidden>Email</label>
-          <input type="text" name="email" id="email" placeholder="Email" required />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="message" hidden>Message</label>
-          <textarea
-            type="text"
-            name="message"
-            id="message"
-            placeholder="Message"
-            required
-          />
-        </div>
-        <input className="hover btn" type="submit" value="Submit" />
-      </form>
-    </section>
+    <div className={styles.container}>
+      <section id="contact" className={styles.container}>
+        <h1 className="sectionTitle">Contact</h1>
+        <form action="" method="post" onSubmit={onSubmit}>
+          <div className="formGroup">
+            <label htmlFor="name" hidden>Name</label>
+            <input type="text" name="name" id="name" placeholder="Name" required />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="email" hidden>Email</label>
+            <input type="text" name="email" id="email" placeholder="Email" required />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="message" hidden>Message</label>
+            <textarea
+              type="text"
+              name="message"
+              id="message"
+              placeholder="Message"
+              required
+            />
+          </div>
+          <input className="hover btn" type="submit" value="Submit" />
+        </form>
+      </section>
+    </div>
   );
 }
 
