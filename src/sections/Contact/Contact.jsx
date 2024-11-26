f9c20cc90c2a6dff817ed83ccea7a8afa29e57ee
Aaron Amoso <amosoaaron@gmail.com>
2024-11-25 18:44:13 -0700

    fixed contact centering

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,9 @@
 import styles from './ContactStyles.module.css';
 
 function Contact() {
-  // Contact API to send emails:
   const onSubmit = async (event) => {
     event.preventDefault();
-    const form = event.target; // Access the form element
+    const form = event.target;
     const formData = new FormData(form);
 
     formData.append("access_key", "b243c422-8eb3-4a42-a515-892013c47add");
@@ -23,28 +22,28 @@ function Contact() {
 
     if (res.success) {
       console.log("Success", res);
-      alert("Your message has been submitted successfully!"); // Alert the user
-      form.reset(); // Reset the form fields
+      alert("Your message has been submitted successfully!");
+      form.reset();
     } else {
       console.log("Failed to send the form", res);
-      alert("Something went wrong. Please try again."); // Notify the user of failure
+      alert("Something went wrong. Please try again.");
     }
   };
 
   return (
-    <div className="contact">
+    <div className={styles.contact}>
       <section id="contact" className={styles.container}>
-        <h1 className="sectionTitle">Contact</h1>
-        <form action="" method="post" onSubmit={onSubmit}>
-          <div className="formGroup">
+        <h1 className={styles.sectionTitle}>Contact</h1>
+        <form action="" method="post" onSubmit={onSubmit} className={styles.form}>
+          <div className={styles.formGroup}>
             <label htmlFor="name" hidden>Name</label>
             <input type="text" name="name" id="name" placeholder="Name" required />
           </div>
-          <div className="formGroup">
+          <div className={styles.formGroup}>
             <label htmlFor="email" hidden>Email</label>
             <input type="text" name="email" id="email" placeholder="Email" required />
           </div>
-          <div className="formGroup">
+          <div className={styles.formGroup}>
             <label htmlFor="message" hidden>Message</label>
             <textarea
               type="text"
@@ -54,7 +53,7 @@ function Contact() {
               required
             />
           </div>
-          <input className="hover btn" type="submit" value="Submit" />
+          <input className={styles.submitButton} type="submit" value="Submit" />
         </form>
       </section>
     </div>
